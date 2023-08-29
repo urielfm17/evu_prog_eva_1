f05f0362b0bfe1ef41b5856711c0f3080aca1910
urielfm17 <143563196+urielfm17@users.noreply.github.com>
2023-08-29 17:50:48 -0600

    Add files via upload

---
+++ EVU1_1_HOLA_MUNDO/src/evu1_1_hola_mundo/EVU1_1_HOLA_MUNDO.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package evu1_1_hola_mundo;
+
+/**
+ *
+ * @author frias
+ */
+public class EVU1_1_HOLA_MUNDO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola mundo");       
+    }
+    
+}
