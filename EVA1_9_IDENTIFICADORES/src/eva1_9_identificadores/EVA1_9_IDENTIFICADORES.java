c2bf53ee5bebe89c4e22af0588292db765c41743
urielfm17 <143563196+urielfm17@users.noreply.github.com>
2023-09-13 17:53:38 -0600

    Add files via upload

---
+++ EVA1_9_IDENTIFICADORES/src/eva1_9_identificadores/EVA1_9_IDENTIFICADORES.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_9_identificadores;
+
+/**
+ *
+ * @author frias
+ */
+public class EVA1_9_IDENTIFICADORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //int 1nombre no es valido porque empieza por numero
+        int $nombre;
+        int _nombre ;
+        int nombre1;       
+        int Nombre;
+        int nombre;
+        int nombre_dato;
+        //no repetir identificadores
+        int Public;
+        
+        
+        
+        
+   
+        
+                
+                
+    }
+    
+}
