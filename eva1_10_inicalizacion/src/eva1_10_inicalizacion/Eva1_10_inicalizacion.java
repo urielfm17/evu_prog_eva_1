146825687f000a84cd56da91f83119542a40d694
urielfm17 <143563196+urielfm17@users.noreply.github.com>
2023-09-18 17:52:06 -0600

    Add files via upload

---
+++ eva1_10_inicalizacion/src/eva1_10_inicalizacion/Eva1_10_inicalizacion.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_10_inicalizacion;
+
+/**
+ *
+ * @author frias
+ */
+public class Eva1_10_inicalizacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       int valor= 0;//inicializando la variable
+        System.out.println(valor);
+        
+        int precio; //declarar
+        precio = 100; // inicializando
+        
+        double salario= 0,preciomayoreo = 100,iva = 10.5,subtotal = 15, total= 100.0;                              
+       
+        //constantes 
+        //final---> te permite asignar un valor solo una vez
+        
+        final int conteo;
+        conteo = 100;
+        System.out.println(conteo);
+        
+        final double MI_VALOR_PI = 3.1416;
+        final string escuela = "imstituto tecnologico de chihuahua ll"
+        final int CALIFICA_PASAR = 70;
+        /* 
+         varias lineas de comentarios
+        cuando quieran tirar mucho rollo 
+        esta es la mejor opcion 
+        */
+     
+        int x;// doble diagonal comentario de una sola linea, todo en la linea es un comentario
+        
+        
+     
+    
+    
+    }
+    
+}
